91e2c7ebcf273caba44cb49f4760591b94d54b65
Rhubarb06150 <matheogass@outlook.com>
2024-07-29 19:54:24 +0200

    push

--- main.py
+++ main.py
@@ -1,17 +1,32 @@
+#zob
+#des que ca baisse bcp il te dit BAM faut acheter
+#zob
+
 import requests
 from bs4 import BeautifulSoup
 from datetime import datetime
-import os
+import time
+
 url='https://crypto.com/price/ethereum'
-r=requests.get(url)
-soup=BeautifulSoup(r.text,'html.parser')
-value=soup.find("span",{"class":"css-13hqrwd"})
-value=value.get_text()
-r_value=str(value)
-r_value=r_value.replace("USD","")
-r_value=r_value.replace("$","")
-r_value=r_value.replace(",","")
-r_value_float=float(r_value)
-print(r_value_float)
-print(datetime.today().strftime('%Y-%m-%d %H:%M:%S'))
-file=open("v/v.txt","w")
+last=0
+while True:
+    r=requests.get(url)
+    soup=BeautifulSoup(r.text,'html.parser')
+    value=soup.find("span",{"class":"css-13hqrwd"})
+    value=value.get_text()
+    r_value=str(value)
+    r_value=r_value.replace("USD","")
+    r_value=r_value.replace("$","")
+    r_value=r_value.replace(",","")
+    r_value_float=float(r_value)
+    if last!=0 and last!=r_value:
+        print('différence')
+    last=r_value
+    #print(r_value_float)
+    date=datetime.today().strftime('%Y-%m-%d-%H-%M-%S')
+    filedate=open(f"v/{date}.txt","w+")
+    filelast=open(f"v/last.txt","w+")
+    filedate.write(r_value)
+    filelast.write(r_value)
+    filedate.close()
+    filelast.close()
